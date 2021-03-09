897fd0fff0419fac980b853f97255c664de3d400
NemesisUA <display.n0ne.01@gmail.com>
2021-03-09 18:49:20 +0200

    feat: solve transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) { throw Error('Not array')};
+
+  let newArr = arr.slice();
+   
+  
+  for (let i = 0; i < arr.length; i++) {
+      switch (arr[i]) {
+          case '--discard-next':
+              if (i < newArr.length - 1) {
+                newArr[i + 1] = 'del';
+              }
+              newArr[i] = 'del';
+              break;
+          case '--discard-prev':
+              if (i > 0) {
+                newArr[i - 1] = 'del';
+              }
+              newArr[i] = 'del';
+              break;
+          case '--double-next':
+              if (i < newArr.length - 1) {
+                newArr[i] = newArr[i + 1];
+              } else {
+                newArr[i] = 'del';
+              }
+              break;
+          case '--double-prev':
+              if (i > 0) {
+                newArr[i] = newArr[i - 1];
+              } else {
+                newArr[i] = 'del';
+              }
+      }
+  }
+
+  return newArr.filter(item => item !== 'del');
 };
