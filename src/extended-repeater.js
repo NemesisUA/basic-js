1c79420e0c23d8225b01d21877d2a954292c794f
NemesisUA <display.n0ne.01@gmail.com>
2021-03-11 22:32:34 +0200

    solve extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  options['separator'] = options['separator'] || '+';
+  options['additionSeparator'] = options['additionSeparator'] || '|';
+  let addition, res;
+  if (options['additionRepeatTimes']) {
+    addition = (options['addition'] + options['additionSeparator']).repeat(options['additionRepeatTimes'] - 1) + options['addition'];
+  } else {
+    addition = options['addition'] || '';
+  }
+  if (options['repeatTimes']) {
+    res = (str + addition + options['separator']).repeat(options['repeatTimes'] - 1) + str + addition;
+  } else {
+    res = str + addition;
+  }
+  return res;
 };
   
